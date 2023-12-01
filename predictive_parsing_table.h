72d15975d40f18ca2758bc9208c730efc8aed719
Christopher Liwanag <cliwanag@csu.fullerton.edu>
2023-12-01 03:21:49 -0800

    changed names of var and consts

--- predictive_parsing_table.h
+++ predictive_parsing_table.h
@@ -1,9 +1,19 @@
 #include <stdexcept>
 #include <map>
-#include <list>
+#include <vector>
 
 #ifndef PREDICTIVE_PARSING_TABLE_H
 #define PREDICTIVE_PARSING_TABLE_H
+
+#define COLS_KEY const std::string&
+#define ROWS_KEY const std::string&
+#define VALUE_SYMBOL const std::string&
+
+struct KeyDoesNotExist : public std::runtime_error
+{
+    KeyDoesNotExist() : std::runtime_error("given key does not exist in parse table") {}
+    KeyDoesNotExist(const std::string& msg) : std::runtime_error(msg) {}
+};
 
 struct TooManyStringObjectsError : public std::runtime_error
 {
@@ -25,34 +35,68 @@ public:
      * 
      * @param rows Number of rows of the parse table
      * @param cols Number of columns of the parse table
-     * @param values Const std::list of const std::string objects where 
-     * these elements are ordered from LEFT to RIGHT and TOP to BOTTOM
+     * @param values Const std::vector of const std::string objects where 
+     * these elements are ordered from left to right and top to bottom;
+     * do not include element (0,0); for empty elements, use NULL
     */
     PredictiveParsingTable(
-        const int rows, 
-        const int cols,
-        const std::list<const std::string&>& stringList) 
+        const size_t rows, 
+        const size_t cols,
+        const std::vector<const std::string>& stringList) 
         : rows{rows}, cols{cols}
     {
-        createMap();
+        createMaps();
         implementParseTable(stringList);
     }
 
     ~PredictiveParsingTable() {
+        delete keyRow_keyCol;
+        delete keyCol_symbolVal;
+    }
 
+    /**
+     * Grab symbol of parse table at indicated position
+     * 
+     * @param col_key Column key string symbol
+     * @param row_key Row key string symbol
+     * @return String symbol at given column and row
+    */
+    VALUE_SYMBOL getSymbolAt(const std::string& col_key, const std::string& row_key) const {
+        
+        try {
+
+        } 
+        catch (...) {
+            throw KeyDoesNotExist();
+        }
     }
 
 private:
-    int rows;
-    int cols;
-    std::map<const std::string&, const std::string&>* parseTable;
+    size_t rows;
+    size_t cols;
+    std::map<const std::string&, const std::string&>* keyRow_keyCol;
+    std::map<const std::string&, const std::string&>* keyCol_symbolVal;
 
-    void createMap() {
-        parseTable = new std::map<const std::string&, const std::string&>;
+    void createMaps() {
+        keyRow_keyCol = new std::map<COLS_KEY, ROWS_KEY>;
+        keyCol_symbolVal = new std::map<ROWS_KEY, VALUE_SYMBOL>;
     }
 
-    void implementParseTable(const std::list<const std::string&>& stringList) {
+    void implementParseTable(const std::vector<const std::string>& stringList) {
+        if (stringList.size() > ((rows * cols)-1))
+            throw TooManyStringObjectsError();
 
+        if (stringList.size() < ((rows * cols)-1))
+            throw TooLittleStringObjectsError();
+
+        for (size_t c = 0; c < cols-1; ++c) {
+            for (size_t r = 0; r < rows-1; ++r) {
+                if (c == 0 && r == 0) continue;
+
+                keyRow_keyCol->insert(std::pair<COLS_KEY,ROWS_KEY>(stringList[0],stringList[0]));
+                keyCol_symbolVal->insert(std::pair<ROWS_KEY,VALUE_SYMBOL>(stringList[0],stringList[0]));
+            }
+        }
     }
 
 };

8dd9e363885c65de16e9fbe620415d5bb8842c9e
Christopher Liwanag <cliwanag@csu.fullerton.edu>
2023-12-01 15:56:42 -0800

    separated stack implementation into functions

--- project2.cpp
+++ project2.cpp
@@ -7,93 +7,90 @@
 // for purposes of this project and since regular symbol ɛ is not supported, use lowercase 'e'
 #define EPSILON "e"
 
+void stackImplementationParse(const PredictiveParsingTable*, std::string);
+std::string reverseStr(std::string);
+
 int main()
 {
     // parse table in vector form and relative information
     const std::vector<const std::string> parseList{"a", "+", "-", "*", "/", "(", ")", "$", "E", "TQ", "", "", "", "", "TQ", "", "", "Q", "", "+TQ", "-TQ", "", "", "", EPSILON, EPSILON, "T", "FR", "", "", "", "", "FR", "", "", "R", "", EPSILON, EPSILON, "*FR", "/FR", "", EPSILON, EPSILON, "F", "a", "", "", "", "", "(E)", "", ""};
     const size_t ROWS{6};
     const size_t COLS{9};
-    const char startSymbol{'E'};
     
     // parse table created using parseList
-    PredictiveParsingTable* parseTable{new PredictiveParsingTable(ROWS, COLS, parseList)};
+    const PredictiveParsingTable* parseTable{new PredictiveParsingTable(ROWS, COLS, parseList)};
 
-    // stack object that holds char types
-    Stack<const char>* charStack{new Stack<const char>};
-
-    // input string
+    // input strings
     std::string inputStr1 = "(a+a)*a$";
     std::string inputStr2 = "a*(a/a)$";
     std::string inputStr3 = "a(a+a)$";
     
-    // stack implementation
-    /**
-     * PUSH $ TO STACK ALWAYS
-     * PUSH STARTING SYMBOL TO STACK
-     * WHILE TOP OF STACK IS NOT EQUAL TO $
-     *      IF FIRST CHAR OF INPUT MATCHES TOP OF STACK
-     *          REMOVE FIRST CHAR FROM INPUT AND UPDATE INPUT
-     *          POP TOP OF STACK
-     *      ELSE
-     *          POP TOP OF STACK
-     *          USE POPPED ELEMENT AND FIRST CHAR OF INPUT AS KEYS TO FIND SYMBOL ON PARSE TABLE
-     *          PUSH THE CHARS OF THE PARSE TABLE ***BACKWARDS*** ONTO THE STACK
-     *      OUTPUT CURRENT STACK
-     * 
-     * note: i do not know the fail condition right now, pls msg me if you have any questions about 
-     * the stack or parse table class, i also do not claim to have the comment implementation completely
-     * correct, much love
-    */
+    std::cout << "==================== PROBLEM 1 ====================" << std::endl;
+    stackImplementationParse(parseTable, inputStr1);
+    std::cout << "==================== PROBLEM 2 ====================" << std::endl;
+    stackImplementationParse(parseTable, inputStr2);
+    std::cout << "==================== PROBLEM 3 ====================" << std::endl;
+    stackImplementationParse(parseTable, inputStr3);
+
+    // free allocated memory
+    delete parseTable;
+
+    return 0;
+}
+
+void stackImplementationParse(const PredictiveParsingTable* parseTable, std::string inputStr) {
+    // stack object that holds char types
+    Stack<const char>* charStack{new Stack<const char>};
+
+    const char startSymbol{parseTable->getStartSymbol()[0]};
 
     std::string topOfStack;
     std::string firstLetterInput;
     std::string parseSymbol;
 
+    // required starting operations
     charStack->push('$');
-    charStack->push(startSymbol);
+    std::cout << "\nInput: " << inputStr << std::endl;
+    std::cout << "Stack: " << "[" << reverseStr(charStack->getString()) << " ]" << std::endl;
 
-    std::cout << charStack->getString() << std::endl;
+    std::cout << "\nInput: " << inputStr << std::endl;
+    charStack->push(startSymbol);
+    std::cout << "Stack: " << "[" << reverseStr(charStack->getString()) << " ]" << std::endl;
+    // end required starting operations
 
     while (charStack->peek() != '$') {
         topOfStack = charStack->peek();
-        firstLetterInput = inputStr1[0];
+        firstLetterInput = inputStr[0];
 
-        std::cout << "\nCOMPARING: " + topOfStack + " " + firstLetterInput << std::endl;
         if (firstLetterInput == topOfStack) { // top of stack DOES match first char of input
-            std::cout << "MATCHES" << std::endl;
             charStack->pop();
 
-            inputStr1 = inputStr1.substr(1,inputStr1.length());
+            inputStr = inputStr.substr(1,inputStr.length());
         }
         else { // top of stack DOES NOT match first char of input
-            std::cout << "DOES NOT MATCH" << std::endl;
             charStack->pop();
 
             parseSymbol = parseTable->getSymbolAt(firstLetterInput, topOfStack);
     
             if (parseSymbol[0] != EPSILON[0]) {
-                std::string reversedParseSymbol(parseSymbol.rbegin(), parseSymbol.rend());
-
-                for (char c : reversedParseSymbol) {
+                for (char c : reverseStr(parseSymbol)) {
                     charStack->push(c);
                 }
             }
         }
-        std::string stackAsString = charStack->getString();
-        std::cout << std::string(stackAsString.rbegin(), stackAsString.rend()) << std::endl;
-        std::cout << inputStr1 << std::endl;
+        std::cout << "\nInput: " << inputStr << std::endl;
+        std::cout << "Stack: " << "[" << reverseStr(charStack->getString()) << " ]" << std::endl;
     }
 
-    if (charStack->peek() == '$' && inputStr1[0] == '$') {
-        std::cout << "string accepted" << std::endl;
-    }
-    else {
-        std::cout << "string not accepted" << std::endl;
-    }
+    if (charStack->peek() == '$' && inputStr[0] == '$')
+        std::cout << "Output: String is accepted/valid." << std::endl;
+    else
+        std::cout << "Output: String is not accepted/invalid." << std::endl;
 
     // free allocated memory
-    delete parseTable;
     delete charStack;
+}
 
-    return 0;
+std::string reverseStr(std::string str) {
+    return std::string(str.rbegin(), str.rend());
 }

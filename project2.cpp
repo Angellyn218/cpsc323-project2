d66d6846224ba9aecf411ea0fbcd56acb1afaef6
Christopher Liwanag <cliwanag@csu.fullerton.edu>
2023-12-01 06:04:47 -0800

    added initial elements

--- project2.cpp
+++ project2.cpp
@@ -3,17 +3,42 @@
 #include <string>
 #include <iostream>
 
-#define given_parse_table = {"a", "+", "-", "*", "/", "(", ")", "$", "E", "TQ", "", "", "", "", "TQ", "", "", "Q", "", "+TQ", "-TQ", "", "", "", "ɛ", "ɛ", "T", "FR", "", "", "", "", "FR", "", "", "R", "", "ɛ", "ɛ", "*FR", "/FR", "", "ɛ", "ɛ", "F", "a", "", "", "", "", "(E)", "", ""}
-
 int main()
 {
+    // parse table in vector form and relative information
     const std::vector<const std::string> parseList{"a", "+", "-", "*", "/", "(", ")", "$", "E", "TQ", "", "", "", "", "TQ", "", "", "Q", "", "+TQ", "-TQ", "", "", "", "ɛ", "ɛ", "T", "FR", "", "", "", "", "FR", "", "", "R", "", "ɛ", "ɛ", "*FR", "/FR", "", "ɛ", "ɛ", "F", "a", "", "", "", "", "(E)", "", ""};
-    PredictiveParsingTable* table{new PredictiveParsingTable(6, 9, parseList)};
+    const size_t ROWS{6};
+    const size_t COLS{9};
 
-    std::cout << table->getSymbolAt("(", "F");
+    // parse table created using parseList
+    PredictiveParsingTable* parseTable{new PredictiveParsingTable(ROWS, COLS, parseList)};
+
+    // stack object that holds char types
+    Stack<const char>* charStack{new Stack<const char>};
+
+    // input string
+    std::string inputStr = "(a+a)$";
 
 
-    delete table;
+    // stack implementation
+    /**
+     * PUSH $ TO STACK ALWAYS
+     * PUSH STARTING SYMBOL TO STACK
+     * WHILE TOP OF STACK IS NOT EQUAL TO $
+     *      IF FIRST CHAR OF INPUT MATCHES TOP OF STACK
+     *          REMOVE FIRST CHAR FROM INPUT AND UPDATE INPUT
+     *          POP TOP OF STACK
+     *      ELSE
+     *          POP TOP OF STACK
+     *          USE POPPED ELEMENT AND FIRST CHAR OF INPUT AS KEYS TO FIND SYMBOL ON PARSE TABLE
+     *          PUSH THE CHARS OF THE PARSE TABLE ***BACKWARDS*** ONTO THE STACK
+     * 
+     * note: i do not know the fail condition right now, pls msg me if you have any questions about the stack or parse table class, much love
+    */
+
+    // free allocated memory
+    delete parseTable;
+    delete charStack;
 
     return 0;
 }
